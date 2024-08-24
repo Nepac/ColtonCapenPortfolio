8c2d6893c214914752eafad552f1c6e57991e6a9
nepac <capencolton@gmail.com>
2024-08-23 22:07:11 -0500

    update

---
+++ portc/src/app/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import ThreeScene from './ThreeScene';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Three.js in React</h1>
+      <ThreeScene />
+    </div>
+  );
+};
+
+export default App;
